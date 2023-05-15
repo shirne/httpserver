d6276f24a418cb8c6c49eea1a74e788f37f2d050
shirne <shirne@126.com>
2023-05-15 23:49:59 +0800

    add error code

--- bin/src/core/middleware.dart
+++ bin/src/core/middleware.dart
@@ -8,23 +8,27 @@ Handler authMiddleware(Handler innerHandler) {
   return (Request request) async {
     final token = request.headers['token'] ?? '';
 
-    final newContext = <String, dynamic>{};
+    final newContext = <String, dynamic>{
+      keyIsLogin: false,
+    };
     if (token.isNotEmpty) {
       final tokenModel = await TokenModule().find(token);
       if (tokenModel != null) {
         if (tokenModel.expireIn + tokenModel.updateTime <
             DateTime.now().millisecondsSinceEpoch / 1000) {
-          return Response.forbidden(Result.error('Token expired'));
+          return Response.forbidden(
+            Result.error('Token expired', code: errorTokenExpire),
+          );
         }
         newContext[keyIsLogin] = true;
 
-        newContext[keyToken] = TokenModule().find(token);
+        newContext[keyToken] = tokenModel;
       } else {
-        return Response.forbidden(Result.error('Invalid token'));
+        return Response.forbidden(
+          Result.error('Invalid token', code: errorTokenInvaild),
+        );
       }
     }
-
-    newContext.putIfAbsent(keyIsLogin, () => false);
 
     return innerHandler(request.change(context: newContext));
   };
--- bin/src/globals.dart
+++ bin/src/globals.dart
@@ -1,6 +1,18 @@
 const keyToken = 'token';
 const keyIsLogin = 'is_login';
 
+const errorNeedLogin = 99;
+const errorLoginFailed = 101;
+const errorNeedVerify = 104;
+const errorNeedRegister = 109;
+const errorRegisterFailed = 111;
+const errorTokenInvaild = 102;
+const errorTokenExpire = 103;
+const errorRefreshTokenInvaild = 105;
+const errorTmpTokenInvaild = 115;
+const errorNeedOpenid = 112;
+const errorMemberDisabled = 113;
+
 typedef Json = Map<String, dynamic>;
 
 typedef DataParser<T> = T? Function(Json);
